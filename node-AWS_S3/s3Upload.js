e49c8dcfeafad08d0bf13b6c25af4f070c7aee0a
micro-hawk <dasvikas0071@gmail.com>
2021-10-06 20:24:08 +0530

    done without buffer content

---
+++ node-AWS_S3/s3Upload.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const AWS = require('aws-sdk')
+const fs = require('fs')
+
+const s3 = new AWS.S3({
+    accessKey : process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY
+})
+
+const uploadSingleFile = (fileLocation, filename ) => {
+    const fileContent = fs.readFileSync(fileLocation)
+
+    const params = {
+        Bucket : process.env.AWS_BUCKET_NAME,
+        Key : filename,
+        Body : fileContent
+    }
+
+    s3.upload(params, (err, data) => {
+        if(err) throw err
+        console.log(`File is successfully uploaded at ${data.Location}`)
+    })
+}
+
+uploadSingleFile('./data/test.pdf', 'sample.pdf')
