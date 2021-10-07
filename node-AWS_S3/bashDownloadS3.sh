9067118d91c035e55ca7f420f46923acdd827bf0
micro-hawk <dasvikas0071@gmail.com>
2021-10-08 02:41:32 +0530

    removed keys

--- node-AWS_S3/bashDownloadS3.sh
+++ node-AWS_S3/bashDownloadS3.sh
@@ -1,13 +1,13 @@
 #!/bin/sh 
 outputFile="./data/downloaded_data/common"
-amzFile="https://vikas-testing-bucket.s3.ap-south-1.amazonaws.com/common.zip"
-bucket="vikas-testing-bucket"
+amzFile=""
+bucket="bucketname"
 resource="/${bucket}/${amzFile}"
 contentType="application/x-compressed-tar"
 dateValue=`date -R`
 stringToSign="GET\n\n${contentType}\n${dateValue}\n${resource}"
-s3Key="AKIAZ4KBV6LA5KWAM3XO"
-s3Secret="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"
+s3Key="access_key"
+s3Secret="secret_key"
 signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
 
 curl  -H "Host: ${bucket}.s3.amazonaws.com" \
